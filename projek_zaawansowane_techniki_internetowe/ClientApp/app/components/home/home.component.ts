1f58f99c80448dea676e487ab2446da605b2af57
Arganx <Kartas345@gmail.com>
2019-03-03 21:10:52 +0100

    M update css
    Dodano css do home page

--- projek_zaawansowane_techniki_internetowe/ClientApp/app/components/home/home.component.ts
+++ projek_zaawansowane_techniki_internetowe/ClientApp/app/components/home/home.component.ts
@@ -3,7 +3,8 @@ import { FormBuilder, FormGroup } from '@angular/forms'
 
 @Component({
     selector: 'home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
     public start: string = "";
